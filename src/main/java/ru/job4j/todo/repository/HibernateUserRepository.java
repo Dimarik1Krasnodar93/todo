e2142e25a4a593c406bcbf33143823cb9d953fef
Dimarik <krasnodar93@mail.ru>
2023-02-16 11:50:03 +0300

    5. Аутентификация в проекте TODO [#504877 #386222]

--- src/main/java/ru/job4j/todo/repository/HibernateUserRepository.java
+++ src/main/java/ru/job4j/todo/repository/HibernateUserRepository.java
@@ -1,11 +1,8 @@
 package ru.job4j.todo.repository;
 
-import org.hibernate.SessionFactory;
 import org.springframework.stereotype.Repository;
 import ru.job4j.todo.model.User;
 
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
@@ -19,16 +16,13 @@ public class HibernateUserRepository implements UserRepository {
 
     @Override
     public Optional<User> add(User user) {
-        crudRepository.run(session -> session.save(user));
-        return Optional.of(user);
+        return crudRepository.crud(user);
     }
 
     @Override
     public Optional<User> findUserByLoginAndPassword(String login, String password) {
-        String hql = "from User where name = :login and password = :password";
-        Map<String, Object> map = Map.of("login", login,
-        "password", password);
-        List<User> list = crudRepository.query(hql, User.class, map);
-        return list.size() > 0 ? Optional.of(list.get(0)) : Optional.empty();
+        String hql = "from User where login = :login and password = :password";
+        Map<String, Object> args = Map.of("login", login, "password", password);
+        return crudRepository.optional(hql, User.class, args);
     }
 }
